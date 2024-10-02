a77a9d08d39b7d9d6d3f9df0ef9cfba63edad891
style77 <stylek777@gmail.com>
2024-10-02 15:38:45 +0200

    refactor(api): remove logs from timer

--- api/internal/utils/position.go
+++ api/internal/utils/position.go
@@ -1,5 +1,11 @@
 package utils
 
+import (
+	"log"
+
+	"github.com/notnil/chess"
+)
+
 func GetPosition(moves []string) string {
 	position := ""
 	for _, move := range moves {
@@ -7,4 +13,31 @@ func GetPosition(moves []string) string {
 	}
 
 	return position
+}
+
+type GameResult struct {
+	Outcome       chess.Outcome
+	OutcomeReason string
+}
+
+func CheckEndGameStates(moves []string) (*GameResult, bool) {
+	board := chess.NewGame()
+
+	for _, move := range moves {
+		board.MoveStr(move)
+	}
+
+	gameEnded := board.Outcome() != chess.NoOutcome
+
+	result := GameResult{
+		Outcome:       board.Outcome(),
+		OutcomeReason: board.Method().String(),
+	}
+
+	log.Println("Game ended with outcome: ", result.Outcome, " Reason: ", result.OutcomeReason)
+	log.Println(gameEnded)
+
+	log.Println(board.String())
+
+	return &result, gameEnded
 }
