aa234919bf253232e9eb42cead5537918cd8d2b2
style77 <stylek777@gmail.com>
2024-10-02 15:24:53 +0200

    chore(api): add timer and color to player struct

--- api/internal/models/player.go
+++ api/internal/models/player.go
@@ -3,6 +3,7 @@ package models
 import (
 	"github.com/gorilla/websocket"
 	"github.com/style77/stockfish-or-not/internal/engine"
+	"github.com/style77/stockfish-or-not/internal/timer"
 )
 
 type Player struct {
@@ -12,4 +13,7 @@ type Player struct {
 	Rank   *int    // ai only
 	Engine *string // ai only
 	AI     *engine.AIManager
+
+	Timer *timer.Timer
+	Color *string
 }
