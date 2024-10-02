bbe95ccc741ae391d1d69a511a3c15989a2d9a39
style77 <stylek777@gmail.com>
2024-10-02 07:35:00 +0200

    chore: constant values

---
+++ api/internal/constants/constants.go
@@ -0,0 +1,14 @@
+package constants
+
+const (
+	AIPlayerPosibility             = 0
+	PlayerLookingIntervalRangeFrom = 4
+	PlayerLookingIntervalRangeTo   = 10
+	TimeoutForOpponent             = 7
+	GameTime                       = 60
+	MaxDepth                       = 10
+
+	// AI
+	AIMoveWaitTimeFrom = 1
+	AIMoveWaitTimeTo   = 6
+)
